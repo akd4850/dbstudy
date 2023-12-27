eebe743dfb5f1b9775a75683187a4533b98b6c83
akd4850 <akd3850@gmail.com>
2023-12-27 12:48:48 +0900

    테이블 삭제

--- 1227_lecture2.sql
+++ 1227_lecture2.sql
@@ -16,5 +16,36 @@
 */
 /*
     데이터타입 종류
+    1. NUMBER(p, s) : 정밀도가 p, 스케일이 s인 숫자
+        1) p : 전체 유효 숫자(글자수)
+        2) s : 소수부의 유효 숫자
+        0.xx - number(2,2)
+        3) 스케일만 생략하면 정수로 표시하는 숫자
+        4) 정밀도와 스케일을 생략하면 정수, 실수 모두 표시할 수 있는 숫자
+    2. CHAR(size) : 글자수가 최대 size인 고정 문자 타입
+        1) 고정 문자 : 글자 수의 변동이 적은 문자(전화번호, 주민번호)
+        2) 최대 size : 2000
+    3. VARCHAR2(size) : 글자수가 최대 size인 가변 문자 타입
+        1) 가변 문자 : 글자 수의 변동이 큰 문자(이름, 주소)
+        2) 최대 size : 4000 byte
+    4. CLOB : VARCHAR2(size)로 처리할 수 없는 큰 문자
+    5. DATE : 날짜/시간(년,월,일,시,분,초)
+    6. TIMESTAMP : 정밀한 날짜/시간(년,월,일,시,분,초,마이크로초1/100만)
+    
     제약조건 종류
+    1. NOT NULL : 필수 입력
+    2. UNIQUE : 중복 불가
+    3. PRIMARY KEY : 기본키
+    4. FOREIGN KEY : 외래키
+    5. CHECK : 작성한 조건으로 값의 제한
 */
+CREATE TABLE BLOG_T (
+    BLOG_NO NUMBER NOT NULL /*UNIQUE*/PRIMARY KEY,
+    TITLE VARCHAR2(100 BYTE) NOT NULL,
+    EDITOR VARCHAR2(100 BYTE) NOT NULL,
+    CONTENTS CLOB /*NULL*/,
+    CREATED DATE NOT NULL
+);
+
+-- 테이블 삭제
+-- DROP TABLE BLOG_T;
